36523081d86587cc6cd7fbfb3da920612f74b909
James Cox <j-cox03@hotmail.com>
2017-07-04 17:28:03 +0100

    Made listener in main activity to recive button clicks and print out word count

--- TextCounter/app/src/main/java/com/example/james/textcounter/MainActivity.java
+++ TextCounter/app/src/main/java/com/example/james/textcounter/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.james.textcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText enterTest = (EditText) findViewById(R.id.enter_text_view_main_activity);
+        Button submitButton = (Button) findViewById(R.id.submit_button_main_activity);
+        final TextView outputView = (TextView) findViewById(R.id.output_view_main_activity);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = enterTest.getText().toString();
+                int wordCount = WordCounter.count(text);
+                outputView.setText(Integer.toString(wordCount));
+            }
+        });
     }
 }
