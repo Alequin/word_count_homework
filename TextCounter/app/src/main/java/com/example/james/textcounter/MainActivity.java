ca78fd6af1e3e4d26bc53295689bc90ec528e565
James Cox <j-cox03@hotmail.com>
2017-07-04 18:54:58 +0100

    Renamed id: submit_button_main_activity => count_button_main_activity

--- TextCounter/app/src/main/java/com/example/james/textcounter/MainActivity.java
+++ TextCounter/app/src/main/java/com/example/james/textcounter/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.james.textcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,10 +15,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final EditText enterTest = (EditText) findViewById(R.id.enter_text_view_main_activity);
-        final Button submitButton = (Button) findViewById(R.id.submit_button_main_activity);
+        final Button countButton = (Button) findViewById(R.id.count_button_main_activity);
         final TextView outputView = (TextView) findViewById(R.id.output_view_main_activity);
 
-        submitButton.setOnClickListener(new View.OnClickListener() {
+        countButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final String text = enterTest.getText().toString();
